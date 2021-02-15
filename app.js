bc0024e22f5177662485a22087aa48ecff7f5bb8
Faaruque <78615569+Faaruque@users.noreply.github.com>
2021-02-15 22:32:29 +0600

    JavaScript Array

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var x = 'Faruque'
-var y = ' Hossain'
-var result = x + y
-console.log(result)
+var country1 = ['Bangladesh','India','Pakistan']
+var country2 = ['USA','Nepal','Austiliya']
+var country = country1.concat(country2);
+
+country.push('Newzelandd');
+country.push('Afganistan');
+country.push('Dhaka','Rongpur')
+console.log(country.length);
+country.pop();
+console.log(country.length)
+console.log(country)
