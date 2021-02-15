673dd475cdf2b5a2aacc75d6191710c4b40f4bbe
Faaruque <78615569+Faaruque@users.noreply.github.com>
2021-02-15 23:25:55 +0600

    Array With for Loop

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-var country1 = ['Bangladesh','India','Pakistan']
-var country2 = ['USA','Nepal','Austiliya']
-var country = country1.concat(country2);
+//Loop for Array.....
 
-country.push('Newzelandd');
-country.push('Afganistan');
-country.push('Dhaka','Rongpur')
-console.log(country.length);
-country.pop();
-console.log(country.length)
-console.log(country)
+var number = [10,20,30,40,50,60]
+
+for(var x = 0; x < 6; x ++){
+    console.log(number[x])
+}
+
+//Loop for Array with sum....
+var num = [10,20,30,40,50]
+var sum = 0;
+for( var x = 0; x < 5; x ++){
+    console.log(num[x]);
+    sum = sum + num [x];
+}
+console.log('TOTAL = ' + sum);
+
+
+//User Input Subtration....
+var num = new Array();
+
+for( var x = 0; x < 5; x ++){
+    num [x] = parseInt(prompt('Enter Any Five Number'));
+}
+
+var sum = 0;
+
+for( var i = 0; i < 5; i ++){
+    console.log(num[x]);
+    sum = sum + num[i];
+}
+console.log('Sum = ' + sum)
